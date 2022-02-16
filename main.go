71ce6eb981f73fd8e183937a561eb24f6cb920f8
himmut85 <67488800+himmut85@users.noreply.github.com>
2022-02-16 16:32:17 +0300

    Update main.go

--- main.go
+++ main.go
@@ -16,6 +16,9 @@ func main() {
 	dbTimeZone := "UTC"
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s", dbHostName, dbUserName, dbUserPass, dbName, dbPort, dbSslMode, dbTimeZone)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err = nil {
+		panic("DB connection established!")
+	}
 	if err != nil {
 		panic("DB connection could not be established!")
 	}
