5cee77ec294f18054605396b2d9d3c9ad488ddfd
himmut85 <67488800+himmut85@users.noreply.github.com>
2022-02-17 09:51:12 +0300

    Update main.go

--- main.go
+++ main.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"log"
-        "net/http"
-        "os"
+	//"log"
+        //"net/http"
+        //"os"
 )
 
 func main() {
@@ -28,10 +28,12 @@ func main() {
 
 	fmt.Println(db)
 	
+	/*
 	port := os.Getenv("PORT")
   	if port == "" {
         	port = "8080"
 	}	
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	*/
 
 }
