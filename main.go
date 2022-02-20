489764eddc17b9458a501f79098e5f7c38a0fb1b
himmut85 <67488800+himmut85@users.noreply.github.com>
2022-02-21 01:20:07 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package main
+/* package main
 
 import (
 	"fmt"
@@ -37,4 +37,43 @@ func main() {
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 	
 
+}
+*/
+
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/denisenkom/go-mssqldb"
+)
+
+var (
+	server   = "10.132.0.43"
+	port     = 1433
+	user     = "testuser"
+	password = "test123"
+)
+
+func main() {
+	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d",
+		server, user, password, port)
+
+	conn, err := sql.Open("mssql", connString)
+	if err != nil {
+		log.Fatal("Open connection failed:", err.Error())
+	}
+	fmt.Printf("Connected!\n")
+	defer conn.Close()
+	stmt, err := conn.Prepare("select @@version")
+	row := stmt.QueryRow()
+	var result string
+
+	err = row.Scan(&result)
+	if err != nil {
+		log.Fatal("Scan failed:", err.Error())
+	}
+	fmt.Printf("%s\n", result)
 }
